2aabd39a089ad9d79b46d74f7e5ee2b2341d1e6c
Taksh <takshformal@gmail.com>
2024-05-14 13:43:43 +0530

    save,reset,get

--- src/app/_components/ToolBar.tsx
+++ src/app/_components/ToolBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Toolbar = ({ selectTool }) => {
   const chooseTool = (e) => {
@@ -7,6 +7,30 @@ const Toolbar = ({ selectTool }) => {
     selectTool(tool);
   };
 
+  useEffect(() => {
+    const svgElement = document.getElementById('svg');
+    const saveButton = document.getElementById('save');
+    let data:string|undefined;
+    saveButton?.addEventListener('click',()=>{
+      console.log(svgElement?.innerHTML  );
+      data=svgElement?.innerHTML;
+    })
+
+    const resetButton=document.getElementById('reset');
+    resetButton?.addEventListener('click',()=>{
+      while(svgElement?.lastChild){
+        svgElement.removeChild(svgElement.lastChild);
+      }
+    })
+
+    const getButton=document.getElementById('get');
+    getButton?.addEventListener('click',()=>{
+      if(svgElement){
+        svgElement.innerHTML=data;
+      }
+    })
+  }, []);
+
   return (
     <div className="flex flex-row justify-center space-x-5 cursor-default">
       <div className="cursor-pointer" onClick={chooseTool}>
@@ -14,6 +38,15 @@ const Toolbar = ({ selectTool }) => {
       </div>
       <div className="cursor-pointer" onClick={chooseTool}>
         Circle
+      </div>
+      <div className='cursor-pointer' id='save'>
+        Save
+      </div>
+      <div className='cursor-pointer' id='reset'>
+        Reset
+      </div>
+      <div className='cursor-pointer' id='get'>
+        Get
       </div>
     </div>
   );
